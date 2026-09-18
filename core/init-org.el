bd2395eab00e20eec5052a7cf37d3245e384e68a
Dragonshock <296568866+Dragonshock@users.noreply.github.com>
2026-09-18 20:51:54 +0800

    Add Org calendar blocks and keep calfw in motion state.
    Wire calfw plus ical holidays from Agenda, remap calendar goto to a
    weekly block view, shrink org-drawer faces with block delimiters, and
    add a conservative-scrolling helper for the calendar buffer.

--- core/init-org.el
+++ core/init-org.el
@@ -123,8 +123,8 @@
           (org-table-align)
           (goto-char point)))))
 
-  ;; Block delimiter faces inherit from `org-meta-line'.
-  (dolist (face '(org-meta-line org-block-begin-line org-block-end-line))
+  ;; Keep drawer and block delimiter faces compact after theme loading.
+  (dolist (face '(org-drawer org-meta-line org-block-begin-line org-block-end-line))
     (set-face-attribute face nil :height 0.85))
 
   ;; Cycle the visible parent heading when point is in or just past folded text.
